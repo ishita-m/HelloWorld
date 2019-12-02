4d6fdcea207936b517db673eddc77852cdbff378
Ishita Mahajan <isha130695@gmail.com>
2019-12-02 23:03:45 +0530

    updated msg

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World from fargate!"
+  return "Hello World deployed using AWS pipeline"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
