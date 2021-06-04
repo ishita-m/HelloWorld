fcf8607f2c3862f67ea120de243ef27784d47634
Ishita Mahajan <ishita@Ishitas-MacBook-Pro.local>
2021-06-04 08:36:29 +0530

    checking if git rebase works

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World deployed using AWS pipeline"
+  return "Hello World deployed using AWS pipeline, making some more changes"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
